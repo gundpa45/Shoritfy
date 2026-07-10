81fae635ae1d29427e5c9c1ad584ed5a0f69ae43
gundpa45 <gundpa45@gmail.com>
2026-07-11 00:49:12 +0530

    utils and services anme

--- Backend/src/utils/youtube.utils.ts
+++ Backend/src/utils/youtube.utils.ts
@@ -1,6 +1,17 @@
 
 
-async function getVideoId(url:string){
+ function getVideoId(url:string){
+
+    const parsedUrl=new URL(url);
+
+    if(!parsedUrl.hostname.includes('youtube.com')){
+        throw new Error('Invalid youtube url'); 
+    }
+
+    const videoId=parsedUrl.searchParams.get("v")
+
+    return videoId 
+
 
 }
 
