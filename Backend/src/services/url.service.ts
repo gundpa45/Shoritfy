1ce59ba36172fbcec80c7ac8d6870f12404999fd
gundpa45 <gundpa45@gmail.com>
2026-07-11 00:30:17 +0530

    helper and business logic

--- Backend/src/services/url.service.ts
+++ Backend/src/services/url.service.ts
@@ -1,11 +1,16 @@
 
-
+import getVideoId from "../utils/youtube.utils.js";
 
 async function urlService(url:string){
 
  
 
     // business logic of the shortify
+    // extract th ervideo id fro mthe vide of friom link 
+
+    const videoId= getVideoId(url)
+
+
 
     return {
         sucess :true
---
+++ Backend/src/utils/youtube.utils.ts
@@ -0,0 +1,8 @@
+
+
+async function getVideoId(url:string){
+
+}
+
+
+export default getVideoId
