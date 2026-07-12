84d0aca25a52324e3f678e5d0c9c5de335806aa9
gundpa45 <gundpa45@gmail.com>
2026-07-12 21:24:56 +0530

    h

--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -4,13 +4,18 @@ dotenv.config()
 if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not defined in .env")
 }
+if(!process.env.YOUTUBE_API_KEY){
+    throw new Error("YOUTUBE_API_KEY is not defined in .env")
+}
 
 type Config = {
     DATABASE_URL: string,
+    YOUTUBE_API_KEY:string,
 }
 
 const config: Config = {
     DATABASE_URL: process.env.DATABASE_URL,
+    YOUTUBE_API_KEY:process.env.YOUTUBE_API_KEY,    
 }
 
 export default config
