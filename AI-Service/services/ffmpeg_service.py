5c1a3f080e954a7c2074c889706cb363fb4c8840
gundpa45 <gundpa45@gmail.com>
2026-07-31 23:47:55 +0530

    hello world

--- AI-Service/services/ffmpeg_service.py
+++ AI-Service/services/ffmpeg_service.py
@@ -1,52 +1,39 @@
 import os
 import subprocess
-from tracemalloc import start
 
 
-def cut_clip(video_path: str, start: float, end: float):
+def cut_clip(
+    video_path: str,
+    start: float,
+    end: float,
+    clip_name: str,
+) -> str:
 
-    # Create output directory
     output_dir = "outputs"
-
     os.makedirs(output_dir, exist_ok=True)
 
-    output_file = os.path.join(output_dir, "clip_1.mp4")
+    output_file = os.path.join(output_dir, f"{clip_name}.mp4")
 
     command = [
         "ffmpeg",
         "-y",
+        "-i", video_path,
+        "-ss", str(start),
+        "-to", str(end),
 
-        "-ss",
-        str(start),
+        "-map", "0:v:0",
+        "-map", "0:a:0",
 
-        "-to",
-        str(end),
+        "-c:v", "libx264",
+        "-preset", "fast",
+        "-crf", "23",
 
-        "-i",
-        video_path,
+        "-c:a", "aac",
+        "-b:a", "192k",
 
-        "-map",
-        "0:v:0",
+        "-movflags", "+faststart",
 
-        "-map",
-        "0:a:0",
-
-        "-c:v",
-        "libx264",
-
-        "-preset",
-        "fast",
-
-        "-crf",
-        "23",
-
-        "-c:a",
-        "aac",
-
-        "-b:a",
-        "192k",
-
-        output_file
+        output_file,
     ]
 
     subprocess.run(command, check=True)
